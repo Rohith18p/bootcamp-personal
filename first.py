bb9c05ed97134e649e69ae982aff051d1ac66889
Rohith18p <rohith18p@gmail.com>
2021-06-03 22:28:27 +0530

    just check

--- first.py
+++ first.py
@@ -1,14 +1,4 @@
 print("om sai ram")
 print("pushed to git")
 
-import math
-import unittest
-import random
 
-def wallis(n):
-    pi = 2.0
-    for i in range(n):
-        pi = pi*((4*(i+1)*(i+1))/(4*(i+1)*(i+1)-1))
-    return pi
-
-wallis(1)
